1c9395ad3a5608c2da1e15e1ff92915038531a43
bosslvlog <154000928+bosslvlog@users.noreply.github.com>
2023-12-16 22:54:39 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Graphics Designer", "Youtuber","Blogger",],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
